5afef5120ec1792de5d36f1ce4cf3d62c678c15a
yansc <yanshichuan1@163.com>
2016-07-30 22:26:09 +0800

    add gittest

---
+++ src/com/git/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git;
+
+/**
+ * @author yanshichuan
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello Git");
+
+	}
+
+}
