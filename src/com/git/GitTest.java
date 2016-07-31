7bd2a748f8c9cfe66122c9fb51e8916e27600a03
yansc <yanshichuan1@163.com>
2016-07-31 15:40:00 +0800

    5th

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -18,6 +18,7 @@ public class GitTest {
 		System.out.println("this a test");
 		System.out.println("this the second test");
 		System.out.println("this the third test");
+		System.out.println("this the 5th test");
 		
 		
 	}
